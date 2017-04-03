fc64e478df0a00e70dac5d155289750951f0ed1c
holystbri01 <briar.holyst@uconn.edu>
2017-04-03 17:54:48 -0400

    Add files via upload

---
+++ game.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Game {
+
+	public Image background;	//whatever background is associated with a given scene
+	public Character char1;		//whatever character is in that scene
+	public TextParser scenetext;	//I need to figure out how the Game file will be saved, what info will be attached to it
+
+//Not quite sure what other characteristics each game file will have
+}
